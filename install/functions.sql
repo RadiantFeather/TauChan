97da2c3fe62a999ae62a4397a420e2269ed59b7a
Techborn <unborntech@gmail.com>
2016-02-18 02:21:33 -0600

    Initial commit for DB schema

---
+++ install/functions.sql
@@ -0,0 +1,266 @@
+﻿-------------------------------------------
+-- Encrypt and validate passwords with crypt-md5
+--
+CREATE OR REPLACE FUNCTION hash_password() RETURNS TRIGGER AS $$
+DECLARE
+	r BOOLEAN;
+BEGIN
+	IF (TG_OP = 'INSERT' OR (TG_OP = 'UPDATE' AND NEW.passphrase IS NOT NULL AND OLD.passphrase = crypt(NEW.passphrase, OLD.passphrase))) THEN
+		NEW.passphrase := crypt(NEW.passphrase,gen_salt('crypt-md5'));
+	END IF;
+END;$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER hash_password
+	BEFORE INSERT OR UPDATE ON users
+	FOR EACH ROW
+	EXECUTE PROCEDURE hash_password();
+	
+CREATE OR REPLACE FUNCTION validate_password(_user VARCHAR(32), _pass VARCHAR(64)) RETURNS BOOLEAN AS $$
+DECLARE
+	r BOOLEAN
+BEGIN
+	SELECT (passphrase = crypt(_pass, passphrase)) INTO r FROM users WHERE username = _user;
+	RETURN r;
+END;$$ LANGUAGE plpgsql;
+
+-------------------------------------------
+-- Compiles post numbers that have cited the requested post into a JSON array
+--
+CREATE OR REPLACE FUNCTION FETCH_cites(_board VARCHAR(32), _thread INTEGER, _post INTEGER) RETURNS JSON AS $$
+DECLARE
+	target RECORD; rv INTEGER[];
+BEGIN
+	FOR target IN SELECT board,thread,post FROM cites WHERE board = _board AND thread = _thread AND cites.targets ? CONCAT_WS('/',_board,_thread,_post) ORDER BY post ASC LOOP
+		rv := rv || target.post;
+	END LOOP;
+	RETURN array_to_json(rv);
+END;$$ LANGUAGE plpgsql;
+
+-------------------------------------------
+-- Compiles media locations into a correctly ordered JSON array
+--
+CREATE OR REPLACE FUNCTION FETCH_media(_board VARCHAR(32), _post INTEGER) RETURNS JSON AS $$
+DECLARE
+	target RECORD; rv TEXT[];
+BEGIN
+	FOR target IN SELECT loc FROM media WHERE board = _board AND post = _post ORDER BY sort ASC LOOP
+		rv := rv || target.loc;
+	END LOOP;
+	RETURN array_to_json(rv);
+END;$$ LANGUAGE plpgsql;
+
+-------------------------------------------
+-- Concats the cite values for proper indexing support
+--
+CREATE OR REPLACE FUNCTION clean_cites() RETURNS TRIGGER AS $$
+DECLARE
+	target RECORD; rv TEXT[];
+BEGIN
+	IF (jsonb_array_length(NEW.targets) > 0) THEN
+		FOR target IN SELECT board,thread,post FROM jsonb_populate_recordset(NULL::TEXT,NEW.targets) LOOP
+			rv := rv || CONCAT_WS('/',target.board,target.thread,target.post);
+		END LOOP;
+		NEW.targets := array_to_jsonb(rv);
+	END IF;
+	RETURN NEW;
+END;$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER clean_cites
+	BEFORE INSERT OR UPDATE ON cites
+	FOR EACH ROW
+	EXECUTE PROCEDURE clean_cites();
+	
+-------------------------------------------
+-- Updates sort field with proper order value
+--
+CREATE OR REPLACE FUNCTION order_media() RETURNS TRIGGER AS $$
+BEGIN
+	SELECT COUNT(1) INTO NEW.sort FROM media WHERE board = NEW.board AND post = NEW.post;
+	RETURN NEW;
+END;$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER order_media
+	BEFORE INSERT ON media
+	FOR EACH ROW
+	EXECUTE PROCEDURE order_media();
+	
+-------------------------------------------
+-- Manage the post sequences since it's relied on per board.
+--
+CREATE OR REPLACE FUNCTION board_seq() RETURNS TRIGGER AS $$
+DECLARE
+	temp BIGINT;
+BEGIN
+	IF (TG_OP = 'INSERT') THEN
+		EXECUTE FORMAT('CREATE SEQUENCE %I MINVALUE 0 OWNED BY boards.board',CONCAT_WS('_',NEW.board,'post','seq'));
+		RETURN NEW;
+	ELSIF (TG_OP = 'DELETE') THEN
+		EXECUTE FORMAT('DROP SEQUENCE IF EXISTS %I',CONCAT_WS('_',OLD.board,'post','seq'));
+		RETURN OLD;
+	ELSIF (TG_OP = 'UPDATE' AND OLD.board <> NEW.board) THEN	--Update post sequence id if board id changes
+		temp := CURRVAL(CONCAT_WS('_',OLD.board,'post','seq'));
+		EXECUTE FORMAT('DROP SEQUENCE %I',CONCAT_WS('_',OLD.board,'post','seq'));
+		EXECUTE FORMAT('CREATE SEQUENCE %I MINVALUE 0 OWNED BY boards.board',CONCAT_WS('_',NEW.board,'post','seq'));
+		PERFORM SETVAL(CONCAT_WS('_',NEW.board,'post','seq'),temp);
+		RETURN NEW;
+	END IF;
+END;$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER board_seq 
+	AFTER INSERT OR DELETE OR UPDATE ON boards
+	FOR EACH ROW
+	EXECUTE PROCEDURE board_seq();
+	
+-------------------------------------------
+-- Proxy function that takes care of managing data for new posts.
+-- 
+CREATE IF NOT EXISTS VIEW post AS 	
+	SELECT _.*, p.*, t.pinned, t.sticky, t.anchor, t.cycle, t.locked, t.sage, t.nsfw 
+	FROM posts p, threads t, (VALUES ('1'::JSON,'1'::JSONB)) AS _(media,cites);
+	
+CREATE OR REPLACE FUNCTION post() RETURNS BOOLEAN AS $$
+DECLARE
+	b RECORD; m RECORD; h JSONB;
+	i BIGINT;
+BEGIN
+	SELECT bumplimit,threadlimit,postlimit,noname INTO b FROM boards WHERE board = NEW.board;
+	SELECT NEXTVAL(CONCAT_WS('_',NEW.board,'post','seq')) INTO NEW.post;
+	IF (NEW.thread IS NULL) THEN NEW.thread := NEW.post; END IF;
+	IF (NEW.name = '') THEN NEW.name := NULL; END IF;
+	NEW.posted := NOW(); --Set manually to update the recent post bump value for the board
+	IF (NEW.media IS NOT NULL) THEN
+		FOR m IN SELECT * FROM json_array_elements(NEW.media) LOOP
+			h := h || m->hash;
+		END LOOP;
+		PERFORM check_media(NEW.board, NEW.thread, array_to_json(h)); -- Validate post media requirements
+	END IF;
+	
+	SELECT COUNT(1) INTO i FROM posts WHERE board = NEW.board AND post <> thread AND capcode IS NULL;
+	IF (NEW.capcode IS NULL AND i >= b.postlimit) THEN
+		RAISE check_violation	--Validate thread reply limit
+			USING MESSAGE = 'New post failed.',
+			DETAIL = 'Thread has reached the reply limit.',
+			CONSTRAINT = 'thread_reply_limit_reached';
+	END IF;
+	
+	INSERT INTO posts(post, thread, board, ip, iphash, name, trip, subject, email, capcode, markdown, markup) VALUES
+		(NEW.post, NEW.thread, NEW.board, NEW.ip, NEW.iphash, NEW.name, NEW.trip, NEW.subject, NEW.email, NEW.capcode, NEW.markdown, NEW.markup);
+	
+	IF (NEW.post = NEW.thread) THEN
+		IF (NEW.pinned = TRUE) THEN	--Only one pinned thread allowed per board and pinned thread must be a sticky
+			UPDATE threads SET pinned = FALSE WHERE board = NEW.board AND pinned = TRUE;
+			NEW.sticky = TRUE;
+		END IF;
+		INSERT INTO threads (op,board,bumped,pinned,sticky,anchor,cycle,locked,sage,nsfw) 
+			VALUES (NEW.thread, NEW.board, NEW.posted, NEW.pinned, NEW.sticky, NEW.anchor, NEW.cycle, NEW.locked, NEW.sage, NEW.nsfw);
+	END IF;
+	
+	UPDATE threads SET archived = NOW() WHERE op IN ( --Archive threads that have fallen past the board thread limit
+		SELECT op FROM threads
+		WHERE board = NEW.board
+		ORDER BY pinned DESC, sticky DESC, anchor DESC, bumped DESC, op DESC
+		OFFSET b.threadlimit
+	);
+	
+	SELECT COUNT(1) INTO i FROM posts WHERE board = NEW.board AND thread = NEW.thread AND post <> NEW.thread;
+	IF (i <= b.bumplimit) THEN
+		UPDATE boards SET bumped = NEW.posted WHERE board = NEW.board; --Update recent post bump for board
+	END IF;
+	IF (NEW.media IS NOT NULL) THEN
+		FOR m IN SELECT * FROM json_to_recordset(NEW.media) AS _(hash TEXT, board VARCHAR(32), thread INTEGER, post INTEGER, loc TEXT); LOOP
+			INSERT INTO media (hash,board,thread,post,loc) VALUES (m.*);
+		END LOOP;
+	END IF;
+	IF (NEW.cites IS NOT NULL) THEN
+		INSERT INTO cites (board,thread,post,targets) VALUES (NEW.board, NEW.thread, NEW.post, NEW.cites);
+	END IF;
+	RETURN TRUE;
+EXCEPTION WHEN OTHERS THEN --Revert post sequence if inserts fail then reraise the exception.
+	PERFORM SETVAL(CONCAT_WS('_',NEW.board,'post','seq'),CURRVAL(CONCAT_WS('_',NEW.board,'post','seq'))-1);
+	RAISE;
+END;$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER post 
+	INSTEAD OF INSERT ON post 
+	FOR EACH ROW 
+	EXECUTE PROCEDURE post();
+
+-------------------------------------------
+-- Pre-new_post-insert check to validate board requirements for media
+--
+CREATE OR REPLACE FUNCTION check_media(_board VARCHAR(32), _thread INTEGER, _hashes JSONB) RETURNS VOID AS $$
+DECLARE
+	found_hash TEXT;
+	b RECORD; i BIGINT;
+BEGIN
+	SELECT imglimit,perthreadunique INTO b FROM boards WHERE board = _board;
+	SELECT hash INTO found_hash FROM media WHERE board = _board AND ((b.perthreadunique AND thread = _thread) OR TRUE) AND _hashes ? hash LIMIT 1;
+	SELECT COUNT(1) INTO i FROM media WHERE board = _board AND thread = _thread;
+	IF (jsonb_array_length(_hashes,1) > 0 AND i + jsonb_array_length(_hashes,1) > b.imglimit) THEN 
+		RAISE check_violation	--Validate thread image limit
+			USING MESSAGE = 'New post failed.',
+			DETAIL = 'Thread has reached the image limit.',
+			CONSTRAINT = 'thread_image_limit_reached';
+	ELSIF (jsonb_array_length(_hashes,1) > 0 AND found_hash) THEN	--Validate image uniqueness
+		RAISE unique_violation
+			USING MESSAGE = 'New post failed.',
+			DETAIL = 'Duplicate image found.',
+			HINT = found_hash,
+			CONSTRAINT = 'duplicate_image_found';
+	END IF;
+END;$$ LANGUAGE plpgsql;
+
+-------------------------------------------
+-- Function for handling a ban user request.
+--
+CREATE OR REPLACE VIEW ban AS
+	SELECT b.board, b.creator, b.reason, b.expires, _.notice, _.range 
+	FROM bans b, (VALUES (0,NULL::VARCHAR(128),0)) AS _(post,notice,range);
+	
+CREATE OR REPLACE FUNCTION ban() RETURNS TRIGGER AS $$
+DECLARE
+	p RECORD;
+	f SMALLINT := 1;
+	g BOOLEAN;
+	-- Might there be a better regex for URL hunting? (this is borrowed from infinity for the time being)
+	r TEXT := '\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))'
+BEGIN
+	IF (NEW.board IS NULL OR NEW.post IS NULL) THEN
+		RAISE not_null_violation;
+	END IF;
+	IF (NEW.range IS NULL) THEN NEW.range := 32; END IF;
+	IF (NEW.creator IS NULL) THEN NEW.creator := 0; END IF;
+	SELECT * INTO p FROM posts WHERE board = NEW.board AND post = NEW.post LIMIT 1;
+	IF (NOT EXISTS(p)) THEN
+		RAISE case_not_found
+			USING MESSAGE = 'Ban and Delete failed.',
+			DETAIL = 'Post does not exist.',
+			CONSTRAINT = 'post_not_found';
+	END IF;
+	SELECT global INTO g FROM boards WHERE board = NEW.board;
+	IF (g) THEN
+		IF (NEW.expires IS NULL OR NEW.expires > '1 year') THEN NEW.expires := '1 year'::INTERVAL; END IF;
+	ELSE 
+		IF (NEW.expires IS NULL OR NEW.expires > '3 months') THEN NEW.expires := '3 months'::INTERVAL; END IF;
+	END IF;
+	IF (family(p.ip) = 6) THEN f := 4; END IF;
+	SELECT * INTO p FROM (
+		VALUES (
+			p.board, p.thread, p.post, p.posted, p.ip,
+			regexp_replace(p.postbody, r, '#Link-Expunged#', 'ix')
+		)
+	) AS _(board,thread,post,posted,ip,postbody);
+	IF (NEW.expires <> '0') THEN -- Set mask length if not a warning
+		p.ip := set_masklen(p.ip,NEW.range*f);
+	END IF;
+	INSERT INTO bans(ip,board,expires,author,reason,post)
+		VALUES (p.ip,NEW.board,NEW.expires,NEW.creator,NEW.reason,to_json(p));
+	IF (NEW.expires <> '0' AND NEW.notice IS NOT NULL) THEN
+		UPDATE posts SET bantext = NEW.notice WHERE board = NEW.board AND post = NEW.post;
+	END IF;
+	RETURN TRUE;
+END;$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER ban
+	INSTEAD OF INSERT ON ban
+	FOR EACH ROW EXECUTE PROCEDURE ban();
---
+++ install/tables.sql
@@ -0,0 +1,344 @@
+﻿CREATE TABLE IF NOT EXISTS boards (
+	board CHARACTER VARYING(32) PRIMARY KEY,
+	title CHARACTER VARYING(64) NOT NULL,
+	listed BOOLEAN DEFAULT TRUE,
+	nsfw BOOLEAN DEFAULT TRUE,
+	created TIMESTAMP DEFAULT NOW(),
+	bumped TIMESTAMP,
+	global BOOLEAN DEFAULT FALSE,
+	lockedlimit SMALLINT DEFAULT 0 CONSTRAINT max_locked_preview_limit CHECK (lockedlimit <= 10),
+	pinnedlimit SMALLINT DEFAULT 1 CONSTRAINT max_pinned_preview_limit CHECK (pinnedlimit <= 10),
+	stickylimit SMALLINT DEFAULT 2 CONSTRAINT max_sticky_preview_limit CHECK (stickylimit <= 10),
+	cyclelimit SMALLINT DEFAULT 3 CONSTRAINT max_cycle_preview_limit CHECK (cyclelimit <= 10),
+	archivedlimit SMALLINT DEFAULT 3 CONSTRAINT max_archived_preview_limit CHECK (archivedlimit <= 10),
+	standardlimit SMALLINT DEFAULT 5 CONSTRAINT max_preview_limit CHECK (standardlimit <= 10),
+	threadlimit SMALLINT DEFAULT 50 CONSTRAINT max_thread_limit CHECK (threadlimit <= 150),
+	bumplimit SMALLINT DEFAULT 150 CONSTRAINT max_bump_limit CHECK (bumplimit <= 500),
+	imagelimit SMALLINT DEFAULT 250 CONSTRAINT max_image_limit CHECK (imagelimit <= 750),
+	postlimit SMALLINT DEFAULT 300 CONSTRAINT max_post_limit CHECK (postlimit <= 1000),
+	archivedlifespan INTERVAL DEFAULT '3 days' CONSTRAINT  max_archived_lifespan CHECK (archivedlifespan <= '7 days'::INTERVAL),
+	perthreadunique BOOLEAN DEFAULT FALSE,
+	publicbans BOOLEAN DEFAULT FALSE,
+	publiclogs BOOLEAN DEFAULT TRUE,
+	loguser BOOLEAN DEFAULT TRUE,
+	preticker VARCHAR(256),
+	postticker TEXT,
+	noname VARCHAR(16) NOT NULL DEFAULT 'Anonymous',
+	tags JSONB
+);
+CREATE INDEX board_tags ON boards USING GIN(tags);
+/*
+SELECT x.*
+	FROM (
+		SELECT b.*, CURRVAL(CONCAT_WS('_', b.board, 'post', 'seq')) AS post_count
+		(SELECT COUNT(1) FROM recent_posts WHERE board = b.board) AS posts_per_hour, 
+		(SELECT COUNT(DISTINCT ip) FROM recent_posts WHERE board = b.board) AS active_users
+		FROM boards b
+		WHERE listed = TRUE
+		AND tags ?& ?::TEXT[] //REQUIRED tags
+		AND tags ?| ?::TEXT[] //OPTIONAL tags
+		AND NOT tags ?| ?::TEXT[] //IGNORE tags
+	) x
+	ORDER BY x.active_users DESC, x.posts_per_hour DESC, x.post_count DESC
+	LIMIT 50 OFFSET (50 * ?) + 50
+	--Board View (unverified) CALLWITH (search.tags.all, search.tags.any, search.tags.none, search.page)
+*/
+
+CREATE TABLE IF NOT EXISTS posts (
+	post INTEGER,
+	thread INTEGER,
+	board VARCHAR(32) NOT NULL REFERENCES boards (board) ON DELETE CASCADE ON UPDATE CASCADE,
+	posted TIMESTAMP NOT NULL DEFAULT NOW(),
+	ip INET NOT NULL DEFAULT '0.0.0.0',
+	iphash CHAR(16) DEFAULT '0000000000000000',
+	edited TIMESTAMP,
+	name VARCHAR(32) NOT NULL,
+	trip VARCHAR(16),
+	subject VARCHAR(128),
+	email VARCHAR(64),
+	saged BOOLEAN DEFAULT FALSE,
+	capcode VARCHAR(64),
+	banned VARCHAR(128),
+	markdown VARCHAR (2048) NOT NULL,
+	markup TEXT,
+	PRIMARY KEY (board, post),
+	FOREIGN KEY (board, thread) REFERENCES posts (board, post) 
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE INDEX post_iphashes ON posts (iphash);
+CREATE INDEX post_boards ON posts (boards);
+CREATE INDEX post_ips ON posts USING GIST (ip INET_OPS);
+CREATE INDEX post_threads ON posts (thread);
+CREATE INDEX post_times ON posts (posted);
+CREATE VIEW recent_posts AS SELECT * FROM posts 
+	WHERE posted >= NOW() - '1 hour'::INTERVAL OFFSET 0; -- Yes this works
+/*
+WITH _ AS (SELECT * FROM (VALUES (?::TEXT,?::INET,?::INT,?::INT)) AS _(board,ip,limit,page))
+SELECT p.* FROM posts p, _
+	WHERE board = _.board AND ip = _.ip
+	ORDER BY posted DESC
+	LIMIT _.limit OFFSET _.page * _.limit + _.limit;
+	--Post History View (unverified) CALLWITH (board.id, user.ip, search.limit, search.page)
+	
+WITH _ AS (SELECT * FROM (VALUES (?,?,?)) AS _(board,limit,page))
+SELECT p.* FROM posts p, _
+	WHERE p.board = _.board ORDER BY posted DESC
+	LIMIT _.limit OFFSET _.limit * _.page + _.limit;
+	--Recent Posts View (unverified) CALLWITH (board.id, search.limit, search.page)
+	
+SELECT x.* 
+	FROM (
+		SELECT p.*, t.pinned, t.sticky, t.anchor, t.cycle, t.locked, t.bumped, t.sage, 
+			(p.post = t.op) AS is_op, cl.local AS local_clean, cl.global AS global_clean, 
+			FETCH_cites(p.board,p.post) AS targets, c.targets AS cites, 
+			FETCH_media(p.board,p.post) AS media
+		FROM posts p, threads t , clean cl, _
+		WHERE p.board = ? AND t.board = p.board AND p.thread = ? AND t.op = p.thread
+			AND cl.board = p.board AND cl.post = p.post
+		ORDER BY (p.post = t.op) DESC p.posted DESC;
+		LIMIT ? + 1
+	) x
+	ORDER BY x.is_op DESC x.posted ASC;
+	--Thread view (verified) CALLWITH (board.id, thread.id, search.replylimit)
+	
+WITH _ AS (SELECT board, stickylimit, cyclelimit, lockedlimit, standardlimit, ?::INTEGER AS page FROM boards WHERE board = ?)
+SELECT x.*
+	FROM (
+		--Fetch Raw Data
+		SELECT ROW_NUMBER() OVER (PARTITION BY p.thread ORDER BY (p.post = t.op) DESC, p.post ASC) AS m, --Enumerate the correct post order per thread
+			COUNT(1) FILTER (WHERE p.post = t.op) OVER (ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS n, --Enumerate the threads
+			p.*, t.pinned, t.sticky, t.anchor, t.cycle, t.locked, t.bumped, t.sage, t.nsfw,
+			cl.local AS local_clean, cl.global AS global_clean, (p.post = t.op) AS is_op,
+			FETCH_cites(p.board,p.post) AS targets, c.targets AS cites, FETCH_media(p.board,p.post) AS media
+		FROM posts p, threads t, cites c, clean cl, _
+		WHERE p.board = _.board AND t.board = p.board AND p.thread = t.op 
+			AND c.board = p.board AND c.post = p.post AND t.archived IS NULL
+			AND cl.board = p.board AND cl.post = p.post
+		ORDER BY t.pinned DESC, t.sticky DESC, t.bumped DESC, p.thread DESC, (p.post = t.op) DESC, p.post ASC --Proper sorting
+	) x, _
+	WHERE (
+		(x.pinned AND x.m <= _.pinnedlimit + 1) --Filter threads for preview limit values
+		OR (x.sticky AND x.m <= _.stickylimit + 1) 
+		OR (x.cycle AND x.m <= _.cyclelimit + 1)
+		OR (x.locked AND x.m <= _.lockedlimit + 1)
+		OR x.m <= _.standardlimit + 1
+		) AND x.n > (_.page * 10) AND x.n <= (_.page * 10) + 10; --LIMIT + OFFSET filter proxy
+	--Index View (unverified) CALLWITH (search.page, board.id)
+	
+WITH _ AS (SELECT board, archivedlimit, archivedlifespan, ? AS page FROM boards WHERE board = ?)
+SELECT x.*
+	FROM (
+		--Fetch Raw Data
+		SELECT ROW_NUMBER() OVER (PARTITION BY p.thread ORDER BY (p.post = t.op) DESC, p.post DESC) AS m, --Enumerate the correct post order per thread
+			COUNT(1) FILTER (WHERE p.post = t.op) OVER (ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS n, --Enumerate the threads
+			p.*, t.archived, t.bumped, (p.post = t.op) AS is_op, FETCH_media(p.board,p.post) as media
+		FROM posts p, threads t, _
+		WHERE p.board = _.board AND p.thread = t.op AND t.archived < NOW() - _.archivedlifespan
+		ORDER BY t.archived DESC, t.bumped DESC, p.thread DESC, (p.post = t.op) DESC, p.post ASC; --Proper sorting
+	) x, _
+	WHERE (x.m <= _.archivedlimit + 1) --Filterd threads for archived preview limit
+	AND x.n > (_.page * 10) AND x.n <= (_.page * 10) + 10; --LIMIT + OFFSET filter proxy
+	--Archive View (unverified) CALLWITH (search.page, board.id)
+	
+UPDATE posts SET archived = NOW()
+	WHERE thread IN (
+		WITH _ AS (SELECT board, threadlimit FROM boards WHERE board = ?)
+		SELECT t.op FROM threads t, _
+		WHERE t.board = _.board
+		ORDER BY t.sticky DESC, t.anchor DESC, t.bumped DESC, t.op DESC
+		OFFSET _.threadlimit
+	);
+	--Vaccuum threads (needs reverified) CALLWITH (board.id)
+	
+DELETE FROM posts
+	WHERE thread IN (
+		WITH _ AS (SELECT board, archivedlimit, perpagelimit FROM boards WHERE b.board = ?)
+		SELECT t.op FROM threads t, _
+		WHERE t.board = _.board AND t.archived IS NOT NULL AND t.archived < NOW() - _.archivedlifespan
+	)
+	RETURNING *;
+	--Vaccuum archive (unverified) CALLWITH (board.id)
+*/
+
+CREATE TABLE IF NOT EXISTS threads (
+	op INTEGER,
+	board VARCHAR(32) NOT NULL,
+	bumped TIMESTAMP NOT NULL,
+	pinned BOOLEAN DEFAULT FALSE, --Topmost thread, only one per board, must be stickied
+	sticky BOOLEAN DEFAULT FALSE, --Keeps thread at the top, required for pinning a thread
+	anchor BOOLEAN DEFAULT FALSE, --Prevents thread from falling off the board
+	cycle BOOLEAN DEFAULT FALSE, --Limits thread post_count, overwrites oldest posts
+	locked BOOLEAN DEFAULT FALSE, --Restricts posting in thread to authorized users only
+	sage BOOLEAN DEFAULT FALSE, --Prevents thread from being bumped to the top
+	nsfw BOOLEAN DEFAULT FALSE, --Auto spoiler all images in the thread, ignored on NSFW boards
+	archived TIMESTAMP, --Marked as fallen off the board into archive state
+	PRIMARY KEY (board, op),
+	FOREIGN KEY (board, op) REFERENCES posts (board, post) 
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
+CREATE INDEX catalog_sort ON threads (board, pinned, sticky, cycle, bumped);
+/*
+WITH _ AS (SELECT board, threadlimit FROM boards WHERE board = ?)
+SELECT t.*, (SELECT COUNT(1) FROM posts p WHERE p.board = t.board AND p.thread = t.op AND p.post <> t.op) AS replies,
+	(SELECT COUNT(1) FROM media m WHERE m.board = t.board AND m.thread = t.op) AS images,
+	COUNT(1) FILTER (WHERE x.n % 10 = 0) OVER (ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS page --Enumerate the pages
+	FROM (
+		SELECT *,COUNT(1) OVER (ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) - 1 AS n --Enumerate the threads
+		FROM threads t, _ WHERE t.board = _.board AND t.archived IS NULL
+		ORDER BY pinned DESC, sticky DESC, bumped DESC
+		LIMIT _.threadlimit
+	) t;
+	--Catalog view (needs reverified) CALLWITH (board.id)
+*/
+
+CREATE TABLE IF NOT EXISTS cites (
+	board VARCHAR(32) NOT NULL,
+	thread INTEGER NOT NULL,
+	post INTEGER NOT NULL,
+	targets JSONB,
+	PRIMARY KEY (board, post),
+	FOREIGN KEY (board, post) REFERENCES posts (board, post) 
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
+CREATE INDEX cite_targets ON cites USING GIN (targets);
+
+CREATE TABLE IF NOT EXISTS media (
+	hash TEXT,
+	board VARCHAR(32),
+	thread INTEGER NOT NULL,
+	post INTEGER NOT NULL,
+	loc TEXT NOT NULL,
+	sort SMALLINT,
+	FOREIGN KEY (board, post) REFERENCES posts (board, post) 
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
+CREATE INDEX media_hash ON media (hash);
+
+CREATE TABLE IF NOT EXISTS users (
+	id SERIAL PRIMARY KEY,
+	username VARCHAR(32) NOT NULL UNIQUE,
+	passphrase VARCHAR(64) NOT NULL,
+	email TEXT UNIQUE,
+	validated BOOLEAN DEFAULT FALSE,
+	global BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+INSERT INTO users VALUES (0,'SYSTEM','','',TRUE); --Passphrase-less user for system driven database operations that require a user
+
+CREATE TABLE IF NOT EXISTS roles (
+	id INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+	board VARCHAR(32) NOT NULL REFERENCES boards (board) ON DELETE CASCADE ON UPDATE CASCADE,
+	role INTEGER NOT NULL,
+	PRIMARY KEY (role, board),
+	CONSTRAINT one_role_per_user_per_board UNIQUE (id, board)
+);
+/*
+SELECT users.*,roles.role FROM users,roles WHERE roles.user = users.id AND roles.board = ?
+*/
+
+CREATE TABLE IF NOT EXISTS bans (
+	ip INET NOT NULL DEFAULT '::',
+	board VARCHAR(32) NOT NULL DEFAULT '_',
+	created TIMESTAMP NOT NULL DEFAULT NOW(),
+	expires INTERVAL NOT NULL CHECK (expires >= '0'),
+	creator INTEGER NOT NULL DEFAULT 0 REFERENCES users (id) ON DELETE SET DEFAULT,
+	reason TEXT NOT NULL DEFAULT 'No Reason Given',
+	seen BOOLEAN NOT NULL DEFAULT FALSE,
+	post JSON,
+	PRIMARY KEY (ip, board),
+	CONSTRAINT ip_is_banned EXCLUDE (ip WITH &&) WHERE (board = board OR board = '_') --Reject bans that are already contained by a range ban (including global bans)
+);
+CREATE INDEX ON bans USING GIST (ip inet_ops);
+
+CREATE TABLE IF NOT EXISTS appeals (
+	ip INET NOT NULL DEFAULT '::',
+	board VARCHAR(32) NOT NULL DEFAULT '_',
+	created TIMESTAMP NOT NULL DEFAULT NOW(),
+	approved TIMESTAMP,
+	approval INTEGER DEFAULT 0 REFERENCES users (id) ON DELETE SET DEFAULT,
+	reason TEXT,
+	FOREIGN KEY (board,ban) REFERENCES bans (board,ip)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT ip_is_not_banned CHECK (ban >>= ip)
+);
+CREATE INDEX ON appeals USING GIST (ip inet_ops);
+/*
+SELECT b.created,b.expires,b.reason,b.post,(b.ip >> a.ip) AS ranged
+	FROM bans b, appeals a
+	WHERE (a.board = ? OR a.board = '_') AND b.board = a.board
+		AND a.ip = ? AND b.ip >>= a.ip
+		AND a.approved IS NOT NULL;
+	--Ban Check (unverified) CALLWITH (board.id, user.ip)
+
+SELECT b.ip,b.board,b.created,b.reason,b.post,a.created AS appealed,a.approved
+	(b.created + b.expires - NOW()) AS expires,(b.ip >> a.ip) AS ranged
+	FROM bans b, appeals a
+	WHERE (a.board = ? OR a.board = '_') AND b.board = a.board 
+		AND a.ip = ? AND b.ip >>= a.ip
+	ORDER BY b.board ASC;
+	--Ban View (unverified) CALLWITH (board.id, user.ip)
+*/
+
+CREATE TABLE IF NOT EXISTS reports (
+	ip INET NOT NULL DEFAULT '::',
+	board VARCHAR(32) NOT NULL DEFAULT '_',
+	created TIMESTAMP NOT NULL DEFAULT NOW(),
+	post INTEGER NOT NULL,
+	dismissed BOOLEAN DEFAULT FALSE,
+	reason VARCHAR(128) NOT NULL DEFAULT 'Unspecified Reason.',
+	FOREIGN KEY (board, post) REFERENCES posts (board, post) 
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
+/*
+SELECT r.post, r.created, r.reason, to_json(p) AS content,
+	FROM posts p, reports r
+	WHERE p.board = ? AND p.board = r.board AND p.post = r.post
+		AND r.dismissed IS FALSE
+	ORDER BY p.post ASC, r.created ASC;
+	--Reports View (unverified) CALLWITH (board.id)
+*/
+
+CREATE TYPE IF NOT EXISTS logaction AS ENUM('access','info','notice','alert');
+CREATE TABLE IF NOT EXISTS logs (
+	board VARCHAR(32) NOT NULL DEFAULT '_',
+	username VARCHAR(32) NOT NULL,
+	created TIMESTAMP NOT NULL DEFAULT NOW(),
+	level LOGACTION NOT NULL,
+	details TEXT
+);
+CREATE INDEX catalog_sort ON threads (board, level);
+/*
+SELECT *
+	FROM (
+		SELECT * FROM logaction
+		WHERE board = ? AND level = ?
+		ORDER BY created DESC
+		LIMIT 100 OFFSET ? * 100
+	) x
+	ORDER BY created ASC;
+	-Mod Logs View (unverified) CALLWITH (board.id, search.level, search.page)
+*/
+
+CREATE TABLE IF NOT EXISTS news (
+	board VARCHAR(32) NOT NULL DEFAULT '_',
+	username VARCHAR(32) NOT NULL,
+	created TIMESTAMP NOT NULL DEFAULT NOW(),
+	edited TIMESTAMP,
+	title VARCHAR(64),
+	markdown VARCHAR(2048)
+	markup TEXT
+);
+
+
+CREATE TABLE IF NOT EXISTS clean (
+	board VARCHAR(32) NOT NULL,
+	post INTEGER NOT NULL,
+	local INTEGER DEFAULT 0 REFERENCES users (id) ON DELETE SET DEFAULT,
+	global INTEGER DEFAULT 0 REFERENCES users (id) ON DELETE SET DEFAULT,
+	PRIMARY KEY (board,post),
+	FOREIGN KEY (board,post) REFERENCES posts (board,post) 
+		ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
